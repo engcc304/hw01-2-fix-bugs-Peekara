9c7fbe52b57b678f08ac5f1cf749a7735cda541b
Peekara <peekarak@gmail.com>
2023-06-29 15:00:20 +0700

    hw02

--- Lab/Lab01-Basic-Display-Input-Output.cpp
+++ Lab/Lab01-Basic-Display-Input-Output.cpp
@@ -3,12 +3,12 @@
 int main() {
     char Name[50] ;
     int  Age = 0 ;
-    printf( "Enter your name: " ); 
-    scanf( "%s", Name ) ;
-    printf( "Enter your age: " ) ;
-    scanf( "%d", &Age ) ;
-    printf( "- - - - - -\n" ) ;
-    printf( "Hello %s \n", name ) ; //TODO: #14 Say hello to user.
-    printf( "Age = %d\n", age) ; //TODO: #15 display user their age number plus 1.
+    printf( "Enter your name: "); 
+    scanf( "%s", Name);
+    printf( "Enter your age: ");
+    scanf( "%d", &Age);
+    printf( "\n");
+    printf( "Hello %s \n", Name); 
+    printf( "Age = %d\n", Age); 
     return 0 ;
-}//end main function
+}
